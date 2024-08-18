21f281a6112ee20557df82b08dfbcbba879df44a
Onyekachukwu Okonji <40724187+onyekaokonji@users.noreply.github.com>
2024-08-18 01:24:30 +0100

    Update run_llava.py

--- llava/eval/run_llava.py
+++ llava/eval/run_llava.py
@@ -142,6 +142,7 @@ if __name__ == "__main__":
     parser.add_argument("--model-path-1", type=str, default="facebook/opt-350m")
     parser.add_argument("--model-path-2", type=str, default="facebook/opt-350m")
     parser.add_argument("--model-base", type=str, default=None)
+    parser.add_argument("--model-name", type=str, default=None)
     parser.add_argument("--image-file", type=str, required=True)
     parser.add_argument("--query", type=str, required=True)
     parser.add_argument("--conv-mode", type=str, default=None)
